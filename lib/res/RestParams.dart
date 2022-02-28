4195eecd836eea1a38ac37b36ff690655a278f3d
mbasa <mario.basa@gmail.com>
2022-02-28 14:02:59 +0900

    modified for Japan data demo.

--- lib/res/RestParams.dart
+++ lib/res/RestParams.dart
@@ -1,3 +1,7 @@
 class RestParams {
-  static final String baseUrl = "http://localhost:8080/pgrServer";
+  static final String baseUrl =
+      "http://marionomac-mini.local/pgrServer"; //"http://localhost:8080/pgrServer";
+
+  static final String defaultBnd =
+      '{"type":"Polygon","coordinates":[[[122.9353314,24.0466948],[122.9353314,45.5222837],[148.5023353,45.5222837],[148.5023353,24.0466948],[122.9353314,24.0466948]]]}';
 }
